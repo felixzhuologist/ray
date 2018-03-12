a782badb49f3c9ab367e36e192dc883e0f633536
felixzhuologist <felix.czhu@gmail.com>
2018-03-12 09:44:46 -0700

    add vector class

--- BUILD
+++ BUILD
@@ -1,4 +1,12 @@
+cc_library(
+  name = "vec3",
+  hdrs = ["vec3.h"]
+)
+
 cc_binary(
   name = "main",
-  srcs = ["main.cc"]
+  srcs = ["main.cc"],
+  deps = [
+    ":vec3",
+  ]
 )
--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "vec3.h"
 
 int main() {
   int nx = 200;
@@ -6,14 +7,9 @@ int main() {
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
   for (int j = ny - 1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
-      float r = float(i) / float(nx);
-      float g = float(j) / float(ny);
-      float b = 0.2;
+      vec3 pixel(float(i) / float(nx), float(j) / float(ny), 0.2);
       // map [0, 1) to [0, 256)
-      int ir = int(255.99 * r);
-      int ig = int(255.99 * g);
-      int ib = int(255.99 * b);
-      std::cout << ir << " " << ig << " " << ib << "\n";
+      std::cout << 255.99 * pixel << "\n";
     }
   }
 }
