50887320f1ccb0629dcd51a291fdcbb4798e2169
felixzhuologist <felix.czhu@gmail.com>
2018-03-13 08:41:28 -0700

    visualize normals

--- main.cc
+++ main.cc
@@ -4,7 +4,8 @@
 vec3 SPHERE_CENTER = vec3(0, 0, -1);
 float SPHERE_RADIUS = 0.5;
 
-// does ray r hit the sphere at given center C and radius r?
+// return value of t where ray r hits the sphere at given center C and radius r,
+// or -1.0 if no solutions exist
 //
 // a point on the ray p(t) is also on the sphere iff:
 // (p(t) - C) · (p(t) - C) = r^2
@@ -12,13 +13,16 @@ float SPHERE_RADIUS = 0.5;
 // (A + tB - C) · (A + tB - C) = r^2
 // t^2|B|^2 + 2t(B · (A-C)) + |A-C|^2 - r^2 = 0
 // (which gives us something of the form: at^2 + bt + c = 0)
-bool hit_sphere(const vec3 &center, float radius, const ray &r) {
+float hit_sphere(const vec3 &center, float radius, const ray &r) {
   vec3 a_minus_c = r.origin() - center;
   float a = dot(r.direction(), r.direction());
   float b = 2 * dot(r.direction(), a_minus_c);
   float c = dot(a_minus_c, a_minus_c) - radius*radius;
   float discriminant = b*b - 4*a*c;
-  return (discriminant > 0);
+  if (discriminant < 0) {
+    return -1.0;
+  }
+  return (-b - sqrt(discriminant)) / (2.0*a);
 }
 
 // Return the background color for a ray, which is a linear interpolation
@@ -31,8 +35,11 @@ vec3 background_color(const ray &r) {
 }
 
 vec3 color(const ray &r) {
-  if (hit_sphere(SPHERE_CENTER, SPHERE_RADIUS, r)) {
-    return vec3(1, 0, 0);
+  float t = hit_sphere(SPHERE_CENTER, SPHERE_RADIUS, r);
+  if (t > 0.0) {
+    // convert normal to a color by getting unit vector and mapping to 0, 1
+    vec3 normal = unit_vector(r.point_at_parameter(t) - SPHERE_CENTER);
+    return 0.5 * vec3(normal.x() + 1, normal.y() + 1, normal.z() + 1);
   }
   return background_color(r);
 }
