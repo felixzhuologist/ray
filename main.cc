adbf8ceef11b43058d1fe7d4822822619db637c2
felixzhuologist <felix.czhu@gmail.com>
2018-03-17 14:52:43 -0700

    add title scene

--- main.cc
+++ main.cc
@@ -33,19 +33,52 @@ vec3 color(const ray &r, hitable *world, int depth) {
   return background_color(r);
 }
 
+hitable *random_scene() {
+  int n = 500;
+  hitable **list = new hitable*[n+1];
+  list[0] =  new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+  int i = 1;
+  for (int a = -11; a < 11; a++) {
+    for (int b = -11; b < 11; b++) {
+      float choose_mat = dis(gen);
+      vec3 center(a + 0.9*dis(gen), 0.2, b + 0.9*dis(gen)); 
+      if ((center - vec3(4, 0.2, 0)).length() > 0.9) { 
+        if (choose_mat < 0.8) {  // diffuse
+          list[i++] = new sphere(center, 0.2, new lambertian(vec3(dis(gen)*dis(gen), dis(gen)*dis(gen), dis(gen)*dis(gen))));
+        }
+        else if (choose_mat < 0.95) { // metal
+          list[i++] = new sphere(center, 0.2, new metal(vec3(0.5*(1 + dis(gen)), 0.5*(1 + dis(gen)), 0.5*(1 + dis(gen))),  0.5*dis(gen)));
+        }
+        else { // glass
+          list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+        }
+      }
+    }
+  }
+  list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+  list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+  list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+  return new bvh_node(list, i);
+}
+
+hitable *small_scene() {
+  hitable *objects[4];
+  objects[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+  objects[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+  objects[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3));
+  objects[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
+  return new bvh_node(objects, 4);
+}
+
 int main() {
   int nx = 200;
   int ny = 100;
   int ns = 100;
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-  camera cam(vec3(0, 0, 1), vec3(0, 0, -1), vec3(0, 1, 0.8), 90, float(nx)/float(ny));
-  hitable *objects[4];
-  objects[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
-  objects[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-  objects[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3));
-  objects[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
-  hitable *world = new bvh_node(objects, 4);
+  // camera cam(vec3(0, 0, 1), vec3(0, 0, -1), vec3(0, 1, 0.8), 90, float(nx)/float(ny));
+  camera cam(vec3(13, 2, 3), vec3(0, 0, 0), vec3(0, 1, 0), 20, float(nx)/float(ny));
+  hitable *world = random_scene();
 
   for (int j = ny - 1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
